1672bf44ab4b4b9f6f6fb106b1772df836b0ef7a
mricomat <maartinricomartinez@gmail.com>
2021-06-25 10:52:43 +0200

    Implemented ui newsListScreen and integrated parser and logic

--- src/screens/newsListScreen.tsx
+++ src/screens/newsListScreen.tsx
@@ -1,15 +1,73 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState, FC } from 'react';
+import { StyleSheet, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { colors, lang } from 'src/assets';
+import Input from 'src/components/input';
+import NewsItem from 'src/components/newsItem';
+import { routeNames } from 'src/hooks/use-navigation';
+import { useRssParser } from 'src/hooks/use-rss-parser';
+import { IRssItem } from 'src/types/data';
+import { RootStackScreenType } from 'src/types/navigation';
+import Config from 'src/utils/config';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.blueTMDB,
+    paddingHorizontal: 8,
   },
 });
 
-const NewListScreen = () => {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+const NewListScreen: FC<RootStackScreenType<routeNames.NewsList>> = ({ navigation }) => {
+  const [searchText, setSearchText] = useState<string>('');
+  const [newsList, setNewsList] = useState<IRssItem[]>([]);
+
+  const { items } = useRssParser(Config.apiUrl, '', true, 24 * 3600000);
+
+  useEffect(() => {
+    setNewsList([...items]);
+  }, [items]);
+
+  useEffect(() => {
+    filterItems();
+  }, [searchText]);
+
+  const filterItems = () => {
+    const newItems = items.filter(item => item.title.toLowerCase().includes(searchText.toLowerCase()));
+    setNewsList([...newItems]);
+  };
+
+  const renderItem = ({ item }: { item: IRssItem }) => (
+    <NewsItem
+      uri={item.enclosures[0].url}
+      title={item.title}
+      description={item.description}
+      style={{ marginBottom: 10 }}
+      onPress={() => navigation.navigate(routeNames.NewsDetail, item)}
+    />
+  );
+
+  const renderList = () => (
+    <FlatList
+      renderItem={renderItem}
+      data={newsList}
+      initialNumToRender={10}
+      keyExtractor={(item, index) => item.id + index}
+    />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Input
+        value={searchText}
+        onChangeText={text => setSearchText(text)}
+        style={{ marginVertical: 12 }}
+        placeholder={lang.searchNewsPlaceHolder}
+      />
+      {renderList()}
+    </SafeAreaView>
+  );
 };
 
 export default NewListScreen;
---
+++ src/utils/config.ts
@@ -0,0 +1,5 @@
+const Config = {
+  apiUrl: `http://www.nasa.gov/rss/dyn/breaking_news.rss`,
+};
+
+export default Config;
